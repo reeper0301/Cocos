be21be1b099c2bf18e5010f70e84afc3ac46bb06
Eun Hyuk Park <reeper0301@gmail.com>
2025-02-06 15:06:30 +0900

    change

--- scripts/movement.ts
+++ scripts/movement.ts
@@ -1,24 +1,40 @@
-import { _decorator, Component, Node, Vec2 } from 'cc';
-const { ccclass, property } = _decorator;
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
-@ccclass('movement')
-export class movement extends Component {
-    private playerPosition: Vec2 = new Vec2();
-    positionXY(event) {
-        this.playerPosition.set(this.node.position.x, this.node.position.y);
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+  
+
+    // LIFE-CYCLE CALLBACKS:
+
+    positionXY(event)
+    {
+        var playerPosition = cc.v2(this.node.position.x,this.node.position.y);
         var mousePosition = event.getLocation();
+
         mousePosition = this.node.parent.convertToNodeSpaceAR(mousePosition);
-        var angle
+        var angle = mousePosition.signAngle(playerPosition);
+        var angleD = cc.misc.radiansToDegrees(angle);
+        angleD = (angleD * -1)-45;
+        this.node.angle = angleD;
     }
-    onLoad() {
-        
-        this.node.parent.on('mousemove',this.positionXY,this);
-    }
-    start() {
+     onLoad () {
+         this.node.parent.on('mousemove',this.positionXY,this);
+     }
+
+    start () {
 
     }
 
-    update(deltaTime: number) {
-        
-    }
+    // update (dt) {}
 }
